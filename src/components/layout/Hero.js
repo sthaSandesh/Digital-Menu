b100ac624c2c35dc209b45af9e1b29fe490831d7
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-05-22 22:26:44 +0545

    home menu and other

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,10 +1,42 @@
-import React from "react";
+import { FaCircleChevronRight } from "react-icons/fa6";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
-export const Hero = () => {
+export default function Hero() {
   return (
-    <section>
-      <h1>Everything is better with a Food</h1>
-      <p>Food is good</p>
+    <section className="hero md:mt-4">
+      <div className="py-8 md:py-12">
+        <h1 className="text-4xl font-semibold">
+          Everything
+          <br />
+          is better
+          <br />
+          with a&nbsp;
+          <span className="text-primary">Pizza</span>
+        </h1>
+        <p className="my-6 text-gray-500 text-sm">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div className="flex gap-4 text-sm">
+          <Button className="flex justify-center bg-primary uppercase items-center gap-2 text-white px-4 py-5 rounded-full">
+            Order now
+            <FaCircleChevronRight />
+          </Button>
+          <button className="flex items-center border-0 gap-2 py-2 text-gray-600 font-semibold">
+            Learn more
+            <FaCircleChevronRight />
+          </button>
+        </div>
+      </div>
+      <div className="relative hidden md:block">
+        <Image
+          src={"/Burger.jpg"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"food"}
+        />
+      </div>
     </section>
   );
-};
+}
