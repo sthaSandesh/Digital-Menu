c34a76cb8be79986f7f020e9a7cedb40735b50e4
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-06-21 20:08:14 +0545

    hero and contact us

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,15 +6,16 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
+          Everything
+          <br />
+          is better
+          <br />
           with a&nbsp;
-          <span className="text-primary">
-            Pizza
-          </span>
+          <span className="text-primary">Food</span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
         </p>
         <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
@@ -28,7 +29,12 @@ export default function Hero() {
         </div>
       </div>
       <div className="relative hidden md:block">
-        <Image src={'/pizza.png'} layout={'fill'} objectFit={'contain'} alt={'pizza'} />
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
       </div>
     </section>
   );
