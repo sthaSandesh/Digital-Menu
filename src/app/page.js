f2db685e7d2c4d5b49a12ee7d108bb2e68c39463
sthaSandesh <126145622+sthaSandesh@users.noreply.github.com>
2024-06-24 08:55:59 +0545

    about update

--- src/app/page.js
+++ src/app/page.js
@@ -14,20 +14,14 @@ export default function Home() {
         <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni
-            minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste
-            quos suscipit tempora? Aperiam esse fugiat inventore laboriosam
-            officiis quam rem!
+            This Project is a digital menu for restaurants. It is a web
+            application that allows customers to view the menu and order food
+            from their table. The application is designed to be used in
+            restaurants, cafes, and bars. It is a simple and easy-to-use
+            application that provides a convenient way for customers to order
+            food.
           </p>
-          <p>
-            At consectetur delectus ducimus est facere iure molestias obcaecati
-            quaerat vitae voluptate? Aspernatur dolor explicabo iste minus
-            molestiae pariatur provident quibusdam saepe?
-          </p>
-          <p>
-            Laborum molestias neque nulla obcaecati odio quia quod reprehenderit
-            sit vitae voluptates? Eos, tenetur.
-          </p>
+          <p>Developed By Sandesh & Sagar</p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
